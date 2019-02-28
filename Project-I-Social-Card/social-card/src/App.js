926fbc12ff3b1a955a37529d6f2d6625ca6ff066
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-02-27 22:26:02 -0600

    Added styling for header and Card content

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import LogoImage from './components/HeaderComponents/ImageThumbnail';
 import HeaderContent from './components/HeaderComponents/HeaderContent';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardBanner from './components/CardComponents/CardBanner';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <div>
+    <div className="bodyContainer">
      
-     <LogoImage/>
-     <HeaderContent/>
+     
+     <HeaderContainer/>
+     <CardContainer/>
+     
     
     
     </div>
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -4,10 +4,10 @@ import './Header.css';
 function HeaderContent(){
     return(
         <div className="profile">
-        <h3>Lambda School<h4>@LambdaSchool 26 jan</h4></h3>
+        <h3>Lambda School<span>@LambdaSchool   26-jan</span></h3>
         
         
-        <div className="headerContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sapiente magni sunt libero error rem commodi minus quasi. Nihil provident a cumque officiis hic asperiores dolores incidunt repellat quibusdam magnam.
+        <div className="headerContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sapiente magni sunt libero error rem commodi minus quasi. Nihil provident a cumque officiis hic asperiores dolores incidunt repellat quibusdam magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, ipsa mollitia quos autem vitae dolor itaque accusantium adipisci sint maiores aspernatur eos sunt fugiat incidunt repudiandae maxime delectus labore atque.
         </div>
         
         
