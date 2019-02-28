caeba1f2d080a17425ed924ed0070e3c7990eaf5
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-02-27 21:15:47 -0600

    Initial commit PR/Added logo and header content components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContent(){
+    return(
+        <div className="profile">
+        <h3>Lambda School<h4>@LambdaSchool 26 jan</h4></h3>
+        
+        
+        <div className="headerContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sapiente magni sunt libero error rem commodi minus quasi. Nihil provident a cumque officiis hic asperiores dolores incidunt repellat quibusdam magnam.
+        </div>
+        
+        
+        </div>
+
+    );
+}
+
+export default HeaderContent;
