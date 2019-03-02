ebd543ad305173c9cfd907f76521501297a9d384
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-03-02 13:31:28 -0600

    Added first row tiles and styling

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton(props) {
+    return(
+        <div className="firstRow">
+            <div className="actionButton"> 
+                <p>clear</p>
+            </div>
+
+            <div className="divideTile">
+                <h2>÷</h2>
+            </div>
+        
+        
+        
+        </div>
+    );
+}
+
+export default ActionButton;
