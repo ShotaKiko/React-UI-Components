cb3900f5c8366a3cc89a3849a44592d655dabc41
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-03-02 12:47:43 -0600

    added calculator container, calcdisplay and the display styling

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+    return (
+      <div className="calculator">
+        <img src={props.student.img} alt={props.student.name} />
+        <div className="firstRow">
+          
+        </div>
+      </div>
+    );
+  }
+  /* 
+  Make sure to console.log your props or use React DevTools to see what data is available to you.
+  */
+  export default NumberButton;
+  
